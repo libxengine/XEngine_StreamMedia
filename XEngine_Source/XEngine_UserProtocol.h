7e654a815946e7777a3400bb145be3ed23344e7a
qyt <486179@qq.com>
2022-04-25 16:53:06 +0800

    update:vs copy and user protocol

--- XEngine_Source/XEngine_UserProtocol.h
+++ XEngine_Source/XEngine_UserProtocol.h
@@ -23,6 +23,18 @@ typedef enum
 //////////////////////////////////////////////////////////////////////////
 #define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_MSG_TEXTREQ 0x0001   //发送消息
 #define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_MSG_TEXTREP 0x0002   //回复消息
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_REQCREATE 0x3001 //创建流
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_REPCREATE 0x3002 
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_REQPUSH 0x3003   //请求推送
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_REPPUSH 0x3004   
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_REQDESTROY 0x3005//请求销毁流
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_REPDESTROY 0x3006
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_REQPULL 0x3007   //请求拉流
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_REPPULL 0x3008      
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_CTRLPLAY 0x3010         //开始播放
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_CTRLPAUSE 0x3011        //暂停播放
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_STATPUSH 0x3012         //推流端状态推送协议
+#define XENGINE_COMMUNICATION_PROTOCOL_OPERATOR_CODE_SMS_STATPULL 0x3013         //拉流端状态推送协议
 ///////////////////////////////////////////////////////////////////////////
 //                          导出的数据结构
 ///////////////////////////////////////////////////////////////////////////
@@ -34,6 +46,12 @@ typedef struct
 	XENGINE_PROTOCOL_AVINFO st_AVInfo;
 	TCHAR tszDeviceNumber[64];
 }XENGINE_PROTOCOLSTREAM;
+typedef struct
+{
+	TCHAR tszDeviceNumber[64];
+	int nChannel;
+	BOOL bLive;
+}XENGINE_PROTOCOLDEVICE;
 //////////////////////////////////////////////////////////////////////////设备协议RTP
 typedef struct
 {
