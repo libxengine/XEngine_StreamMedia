f0175d305abc399a4705e5e8f18c9d8354e4b981
qyt <486179@qq.com>
2025-01-03 10:12:56 +0800

    update:example x64 build support

---
+++ XEngine_APPClient/VSCopy-x64.bat
@@ -0,0 +1,22 @@
+copy /y "%XEngine_Lib64%\XEngine_BaseLib\XEngine_BaseLib.dll" "./"
+
+copy /y "%XEngine_Lib64%\XEngine_Core\XEngine_Cryption.dll" "./"
+
+copy /y "%XEngine_Lib64%\XEngine_Client\XClient_Socket.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_Client\XClient_APIHelp.dll" "./"
+
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\XEngine_AVHelp.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\XEngine_AVCollect.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\XEngine_VideoCodec.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\XEngine_AVFrame.dll" "./"
+
+copy /y "%XEngine_Lib64%\XEngine_StreamMedia\StreamMedia_SDPProtocol.dll" "./"
+
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\avcodec-60.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\avdevice-60.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\avfilter-9.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\avformat-60.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\avutil-58.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\postproc-57.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\swresample-4.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_AVCodec\swscale-7.dll" "./"
