e88ad059d5d79f7a29edda39da48f22edfb1f02e
qyt <486179@qq.com>
2026-09-05 11:03:02 +0800

    update:depend library update:vscopy file fxied:build error

--- XEngine_Source/XEngine_ServiceApp/XEngine_StreamMediaApp/XEngine_Hdr.h
+++ XEngine_Source/XEngine_ServiceApp/XEngine_StreamMediaApp/XEngine_Hdr.h
@@ -63,7 +63,6 @@ using namespace std;
 #include <XEngine_Include/XEngine_StreamMedia/RTCPProtocol_Error.h>
 #include <XEngine_Include/XEngine_AVCodec/VideoCodec_Define.h>
 #include <XEngine_Include/XEngine_AVCodec/AudioCodec_Define.h>
-#include <XEngine_Include/XEngine_AVCodec/AVCollect_Define.h>
 #include <XEngine_Include/XEngine_AVCodec/AVHelp_Define.h>
 #include <XEngine_Include/XEngine_AVCodec/AVHelp_Error.h>
 #include <XEngine_Include/XEngine_SystemSdk/SystemApi_Define.h>
